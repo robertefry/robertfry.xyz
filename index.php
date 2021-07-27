990e8308bf8b079346f99534fc29eff40725c00a
Robert E Fry <robert.e.fry@protonmail.com>
2021-07-27 22:41:25 +0100

    Move common html blocks to `common` and include with php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+  <?php include("assets/common/header.htm"); ?>
+  <main>
+    <?php include("assets/common/dashboard.htm"); ?>
+    <div style="padding-top: 1rem;">
+      <h3>About Me</h3>
+      <hr>
+      <p>This website is currently under construction, please be patient.</p>
+    </div>
+  </main>
+  <?php include("assets/common/footer.htm"); ?>
+</body>
+</html>
