750f46228a5147f0ced62bbe44dd62701e399feb
Robert E Fry <robert.e.fry@protonmail.com>
2021-10-07 03:41:03 +0100

    Add motd

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
 <body>
   <?php include("assets/common/header.htm"); ?>
   <main>
+    <div class="motd">
+      Where fall the footsteps of the master, the ears of those ready for his teaching open wide.<br>
+      When the ears of the student are ready to hear, then cometh the lips to fill them with wisdom.<br>
+    </div>
     <?php include("assets/common/dashboard.htm"); ?>
     <div class="contents">
       <h3>About Me</h3>
